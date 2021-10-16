9e533e482ab87d6bd337bf1d5a9ddabfc7d4ebb8
Kian G <kian.gormley13@aol.com>
2021-10-16 19:28:13 -0400

    win and no repeats
    user can no longer repeat boxes and the winning conditions are now established

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 const playerOne = 'X';
 const playerTwo = 'O';
 let playerMove = 1;
-let board = ['', '', '', '', '', '', '', '', '']
-
-const comboOne = []
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
 
 // const winPatterns = {
 //   win1: [#col-1a, #col-1b, #col-1c]
@@ -16,36 +15,79 @@ const comboOne = []
 //   win8: [#col-1c, #col-2b, #col-3a]
 // }
 
-
 const authEvents = require('./auth/events');
 
 const turns = function (event) {
   console.log(event.target.id);
-  if (playerMove % 2 === 1) {
-    $(`#${event.target.id}`).html(playerOne);
-    playerMove++
-  } else {
-    $(`#${event.target.id}`).html(playerTwo);
-    playerMove++
+  if ($(event.target).html() === '') {
+    if (playerMove % 2 === 1) {
+      $(`#${event.target.id}`).html(playerOne);
+      playerMove++;
+      currentPlayer = playerOne;
+    } else {
+      $(`#${event.target.id}`).html(playerTwo);
+      playerMove++;
+      currentPlayer = playerTwo;
+    }
+    boardArray(event.target.id);
+    winningCombos()
   }
-  noRepeat(event)
-}
+};
 
-const boardArray = function() {
-  if (playerMove % 2 === 1) {
-
+const boardArray = function (id) {
+  if (id === 'col-1a') {
+    board[0] = currentPlayer;
+  } else if (id === 'col-1b') {
+    board[1] = currentPlayer;
+  } else if (id === 'col-1c') {
+    board[2] = currentPlayer;
+  } else if (id === 'col-2a') {
+    board[3] = currentPlayer;
+  } else if (id === 'col-2b') {
+    board[4] = currentPlayer;
+  } else if (id === 'col-2c') {
+    board[5] = currentPlayer;
+  } else if (id === 'col-3a') {
+    board[6] = currentPlayer;
+  } else if (id === 'col-3b') {
+    board[7] = currentPlayer;
+  } else if (id === 'col-3c') {
+    board[8] = currentPlayer;
   }
-}
+  console.log(board);
+};
 
-const noRepeat = function (event) {
-  if ($(`#${event.target.id}`).html() === 'X' || $(`#${event.target.id}`).html() === 'O') {
-    $(`#${event.target.id}`).off('click')
-    console.error('You cannot select this box')
-  }
-}
+// const noRepeat = function (event) {
+//   if (
+//     $(`#${event.target.id}`).html() === 'X' ||
+//     $(`#${event.target.id}`).html() === 'O'
+//   ) {
+//     $(`#${event.target.id}`).off('click');
+//     console.error('You cannot select this box');
+//   }
+// };
 
 const winningCombos = function () {
-
+  console.log(board);
+  if (board[0] === board[1] && board[0] === board[2] && board[0] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[3] === board[4] && board[3] === board[5] && board[3] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[6] === board[7] && board[6] === board[8] && board[6] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[0] === board[3] && board[0] === board[6] && board[0] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[1] === board[4] && board[1] === board[7] && board[1] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[2] === board[5] && board[2] === board[8] && board[2] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[0] === board[4] && board[0] === board[8] && board[0] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else if (board[2] === board[4] && board[2] === board[6] && board[2] !== '') {
+    console.log(currentPlayer, 'Wins!');
+  } else {
+    console.log('Its a tie!')
+  }
 }
 
 // const noTurn = function () {
@@ -57,7 +99,7 @@ const winningCombos = function () {
 // }
 
 const playAgain = function () {
-  playerMove = 1
+  playerMove = 1;
   // $('#col-1a').html('').one('click', turns);
   // $('#col-1b').html('').one('click', turns);
   // $('#col-1c').html('').one('click', turns);
@@ -77,15 +119,14 @@ const playAgain = function () {
   $('#col-3a').html('');
   $('#col-3b').html('');
   $('#col-3c').html('');
-}
+  board = ['', '', '', '', '', '', '', '', ''];
+};
 
 const xWins = function () {
   if ('#col-1a, #col-1b, #col-1c' === 'x') {
-    $('#')
-
+    $('#');
   }
-}
-
+};
 
 $(() => {
   // your JS code goes here
@@ -95,8 +136,8 @@ $(() => {
   $('#sign-out').on('submit', authEvents.onSignOut);
   $('#new-game').on('click', authEvents.onNewGame);
   $('#play-again').on('click', playAgain);
-  $('.before-new-game').hide()
-
+  $('.before-new-game').hide();
+  playAgain()
   $('#box').on('click', turns);
   // $('#box').on('click', noRepeat);
 
@@ -109,9 +150,6 @@ $(() => {
   // $('#col-3a').one('click', turns);
   // $('#col-3b').one('click', turns);
   // $('#col-3c').one('click', turns);
-
-
-
 
   //   $('#col-1a').on('click', () => {
   //     $('#col-1a').text('X')
