ab41dd5abbf35b8da423c769f082eaea4a1cd3b0
Kian G <kian.gormley13@aol.com>
2021-10-16 21:08:03 -0400

    cleaned up app.js comments

--- app/app.js
+++ app/app.js
@@ -5,17 +5,6 @@ let currentPlayer = 'X';
 let board = ['', '', '', '', '', '', '', '', ''];
 let gameOver = true
 
-// const winPatterns = {
-  //   win1: [#col-1a, #col-1b, #col-1c]
-  //   win2: [#col-2a, #col-2b, #col-2c]
-//   win3: [#col-3a, #col-3b, #col-3c]
-//   win4: [#col-1a, #col-2a, #col-3a]
-//   win5: [#col-1b, #col-2b, #col-3b]
-//   win6: [#col-1c, #col-2c, #col-3c]
-//   win7: [#col-1a, #col-2b, #col-3c]
-//   win8: [#col-1c, #col-2b, #col-3a]
-// }
-
 const authEvents = require('./auth/events');
 
 const turns = function (event) {
@@ -58,17 +47,7 @@ const boardArray = function (id) {
   console.log(board);
 };
 
-// const noRepeat = function (event) {
-//   if (
-  //     $(`#${event.target.id}`).html() === 'X' ||
-//     $(`#${event.target.id}`).html() === 'O'
-//   ) {
-  //     $(`#${event.target.id}`).off('click');
-//     console.error('You cannot select this box');
-//   }
-// };
 const playerWon = function () {
-
 
 }
 
@@ -99,26 +78,8 @@ const winningCombos = function () {
   }
 }
 
-// const noTurn = function () {
-//   if (event.target.id === 'x') {
-//     $(`#${event.target.id}`).html(playerOne)
-//   } else if ($(event.target.id === 'o') {
-//     $(`#${event.target.id}`).html(playerTwo)
-//   }
-// }
-
 const playAgain = function () {
   playerMove = 1;
-  // $('#col-1a').html('').one('click', turns);
-  // $('#col-1b').html('').one('click', turns);
-  // $('#col-1c').html('').one('click', turns);
-  // $('#col-2a').html('').one('click', turns);
-  // $('#col-2b').html('').one('click', turns);
-  // $('#col-2c').html('').one('click', turns);
-  // $('#col-3a').html('').one('click', turns);
-  // $('#col-3b').html('').one('click', turns);
-  // $('#col-3c').html('').one('click', turns);
-
   $('#col-1a').html('');
   $('#col-1b').html('');
   $('#col-1c').html('');
@@ -129,12 +90,6 @@ const playAgain = function () {
   $('#col-3b').html('');
   $('#col-3c').html('');
   board = ['', '', '', '', '', '', '', '', ''];
-};
-
-const xWins = function () {
-  if ('#col-1a, #col-1b, #col-1c' === 'x') {
-    $('#');
-  }
 };
 
 $(() => {
@@ -148,19 +103,4 @@ $(() => {
   $('.before-new-game').hide();
   playAgain()
   $('#box').on('click', turns);
-  // $('#box').on('click', noRepeat);
-
-  // $('#col-1a').one('click', turns);
-  // $('#col-1b').one('click', turns);
-  // $('#col-1c').one('click', turns);
-  // $('#col-2a').one('click', turns);
-  // $('#col-2b').one('click', turns);
-  // $('#col-2c').one('click', turns);
-  // $('#col-3a').one('click', turns);
-  // $('#col-3b').one('click', turns);
-  // $('#col-3c').one('click', turns);
-
-  //   $('#col-1a').on('click', () => {
-  //     $('#col-1a').text('X')
-  //   })
 });
